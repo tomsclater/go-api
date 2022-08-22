af52937a7af0d85ddcd4f2ccbf3ad5d87669e9ba
Thomas <tomsclater@live.com>
2022-08-21 22:13:27 -0700

    Setting up routes

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// import(
+// 	"fmt"
+// 	"log"
+// 	"encoding/json"
+// 	"math/rand"
+// 	"net/http"
+// 	"strconv"
+// 	"github.com/gorilla/mux"
+// )
+
+
 type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
@@ -10,4 +27,21 @@ type Movie struct {
 type Director struct {
 	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main(){
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn:"438227", Title :"Movie One", Director : &Director{Firstname:"John", Lastname:"Doe"}})
+	movies = append(movies, Movie{ID:"2", Isbn: "45455", Title:"Movie Two", Director: &Director{Firstname:"Steve", Lastname:"Smith"}})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000",r))
 }
