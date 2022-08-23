5d77784ae9825564cf71101d072d57ec0c1378b3
tomsclater <tomsclater@live.com>
2022-08-23 03:21:22 -0700

    Finished update movie function

--- main.go
+++ main.go
@@ -65,15 +65,19 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 
 func updateMovie()
 {
-	// set json content type
 	w.Header().Set("Content-Type", "application/json")
-	// params
 	params := mux.Vars(r)
-	// loop over the movies, range
-	// delete the movie with the id you sent
-	// add new movie - send in body of postman
 
-	
+	for index, item := range movies{
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
 }
 
 func main() {
